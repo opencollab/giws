a3564ebd9f8abd632617cae963989203316b323f
ledru <ledru@50652a54-1d3b-0410-afef-fc1381fdeda4>
2007-09-27 13:06:22 +0000

    an example of the use og giws

---
+++ examples/example1/example/MyObject.java
@@ -0,0 +1,16 @@
+package example;
+
+public class MyObject{
+	
+	public MyObject(){
+		System.out.println("Constructor");
+	}
+
+	public String GetMyString(){
+		return "I do love Scilab";
+	}
+	
+	public void DoNothingPleaseButDisplay(int a){
+		System.out.println("I did display some very interesting things like the int : "+a);
+	}
+}
---
+++ examples/example1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "example.hxx"
+#include <jni.h>
+
+JNIEnv* create_vm() {
+	JavaVM* jvm;
+	JNIEnv* env;
+	JavaVMInitArgs args;
+	JavaVMOption options[2];
+	
+	/* There is a new JNI_VERSION_1_4, but it doesn't add anything for the purposes of our example. */
+	args.version = JNI_VERSION_1_6;
+
+	args.nOptions = 2;
+	options[0].optionString = "-Djava.class.path=.";
+	options[1].optionString = "-Xcheck:jni";
+	args.options = options;
+	args.ignoreUnrecognized = JNI_FALSE;
+
+	JNI_CreateJavaVM(&jvm, (void **)&env, &args);
+	return env;
+}
+
+using namespace example;
+using namespace std;
+
+int main(){
+  	JNIEnv* env = create_vm();
+	MyObject *plop = new MyObject(env);
+	plop->DoNothingPleaseButDisplay(23);
+	char * p = plop->GetMyString();
+	cout << "crash :" << plop->GetMyString() <<endl;
+	return 0;	
+}
