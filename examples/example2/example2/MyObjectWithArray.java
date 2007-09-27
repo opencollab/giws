148a53236899c066fa977a87515f0eebef192246
ledru <ledru@50652a54-1d3b-0410-afef-fc1381fdeda4>
2007-09-27 13:11:08 +0000

    a new example with array

---
+++ examples/example2/example2/MyObjectWithArray.java
@@ -0,0 +1,27 @@
+package example2;
+
+public class MyObjectWithArray{
+	
+	public MyObjectWithArray(){
+		System.out.println("Constructor");
+	}
+
+	public String[] getMyString(){
+		String[] arrayOfString={ "I do love Scilab","Don't you ?" };
+		return arrayOfString;
+	}
+	
+	public void doNothingPleaseButDisplay(int[] a, short[] b){
+		System.out.println("Your first array has a size of "+a.length+" elements and the second " +b.length);
+        for( int i=0; i<a.length; i++)
+			System.out.println("I did display some very interesting things like the int Array : "+a[i]);
+        for( int i=0; i<b.length; i++)
+			System.out.println("I did display some very uninteresting things like the short Array : "+b[i]);
+	}
+
+	public static void main(String []args){
+		MyObjectWithArray plop = new MyObjectWithArray();
+		plop.getMyString();
+
+	}
+}
---
+++ examples/example2/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "example2.hxx"
+#include <jni.h>
+
+JavaVM* create_vm() {
+	JavaVM* jvm;
+	JNIEnv* env;
+	JavaVMInitArgs args;
+	JavaVMOption options[2];
+	
+	/* There is a new JNI_VERSION_1_4, but it doesn't add anything for the purposes of our example. */
+	args.version = JNI_VERSION_1_6;
+
+	args.nOptions = 2;
+	options[0].optionString = "-Djava.class.path=.";
+	options[1].optionString = "-Xcheck:jni";
+	args.options = options;
+	args.ignoreUnrecognized = JNI_FALSE;
+
+	JNI_CreateJavaVM(&jvm, (void **)&env, &args);
+	return jvm;
+}
+
+using namespace example2;
+using namespace std;
+
+int main(){
+	int sizeArray=3;
+  	JavaVM* jvm = create_vm();
+	MyObjectWithArray *plop = new MyObjectWithArray(jvm);
+	long myArrayOfLong[sizeArray];
+	myArrayOfLong[0]=42;
+	myArrayOfLong[1]=69;
+	myArrayOfLong[2]=12;
+	short myArrayOfShort[sizeArray+1];
+	myArrayOfShort[0]=4;
+	myArrayOfShort[1]=6;
+	myArrayOfShort[2]=1;
+	myArrayOfShort[3]=1;
+	plop->doNothingPleaseButDisplay(myArrayOfLong,sizeArray,myArrayOfShort, sizeArray+1 );
+
+	char ** myString=plop->getMyString();
+	cout << "The first string from Java : " << myString[0]  <<endl;
+	cout << "The second string from Java : " << myString[1]  <<endl;
+	return 0;	
+}
